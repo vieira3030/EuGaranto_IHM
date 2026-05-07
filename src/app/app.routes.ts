4ec9a0dfda1b115bb5d48c8189a0a44a112e43ce
vieira3030 <vieirarodrigo@ipvc.pt>
2026-05-07 15:30:22 +0100

    fix(Rodrigo): garantir sincronização de todos os ficheiros de rotas e grupos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    // Rota inicial que carrega o sistema de tabs da aplicação
+   // Ficheiro principal de rotas da aplicação (EuGaranto)
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
